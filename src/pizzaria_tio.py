13fe235ebe7c6bde45dc84db9c448a6159f85979
AlmirFonseca <almirfonseca60@gmail.com>
2022-10-14 08:15:10 -0300

    Defina a assinatura das funções

--- src/pizzaria_tio.py
+++ src/pizzaria_tio.py
@@ -23,3 +23,23 @@
 #Cobertura           ----   
 
 #Monta pizzas, Cadastra Ingredientes, Remove Ingredientes, Lista Ingredientes (+++)
+
+def listar_ingredientes(ingredientes, tipo=""):
+
+    # imprime a lista de ingredientes e retorna a lista de ingrediente selecionada/filtrada
+    pass
+
+def cadastrar_ingrediente(ingredientes, tipo ,nome, valor):
+
+    # Retorna o dataframe atualizado, mas imprime o ingrediente adicionado
+    pass
+
+def remover_ingrediente(ingredientes, nome_ingrediente):
+
+    # Retorna o dataframe atualizado, mas imprime o ingrediente removido
+    pass
+
+def montar_pizza(ingredientes, massa, molho, queijo, cobertura):
+    
+    # retorna o valor da pizza, mas tambem imprimir o conteúdo/preço da pizza
+    pass
---
+++ teste.py
@@ -0,0 +1,11 @@
+def listar_ingredientes():
+    pass
+
+def cadastrar_ingrediente():
+    pass
+
+def remover_ingrediente():
+    pass
+
+def montar_pizza():
+    pass
