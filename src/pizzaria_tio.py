687754c3a3a31382563640a254ad1dc12610b0c3
Abner PCV <abnerlucas.pcv.12@gmail.com>
2022-10-14 08:09:02 -0300

    Adicione pastas src e test, e arquivos pizzario_tio.py e test.py

---
+++ src/pizzaria_tio.py
@@ -0,0 +1,25 @@
+#ingredientes em um dataframe
+
+
+#Pizza
+######################################
+#Tipo           Ingredientes  Valor
+#Massa      Tradicional     1
+#Molho      Tomate          1
+#Queijo     Mussarela       1 
+#Cobertura  Azeitona        1
+#Total                      4
+
+#Ingredientes
+########################################################
+#Tipo                    Nome        Valor       
+#Massa              Tradicional  5            
+#Massa               ----        4            
+#Molho               ----        -  
+#Molho               ----
+#Queijo              ----
+#Queijo              ----   
+#Cobertura           ----
+#Cobertura           ----   
+
+#Monta pizzas, Cadastra Ingredientes, Remove Ingredientes, Lista Ingredientes (+++)
