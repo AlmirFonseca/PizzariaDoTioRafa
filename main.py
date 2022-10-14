9fb704a04ffbd4ee98f9d816a8cdadd42db5235b
AlmirFonseca <almirfonseca60@gmail.com>
2022-10-14 09:16:26 -0300

    Finalize a main.py

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ linhas = [["Massa", "Tradicional", 5.0], ["Massa", "Fina", 5.0],
 
 ingredientes = pd.DataFrame(data=linhas, columns=colunas)
 
+# Dataframe de ingredientes
 print(ingredientes)
 
-print(pm.montar_pizza(ingredientes=ingredientes, massa="NAOEXISTE", molho="NAOEXISTE", queijo="NAOEXISTE", cobertura="NAOEXISTE"))
+# Função de listar ingredientes
+print(pm.listar_ingredientes(ingredientes, "Massa"))
+
+# Função de cadastrar ingredientes
+print(pm.cadastrar_ingrediente(ingredientes, "Queijo", "Cheddar", 10.0))
+
+# Função de remover ingredientes
+print(pm.remover_ingrediente(ingredientes, "Tradicional"))
+
+# Função de montar pizza
+print(pm.montar_pizza(ingredientes=ingredientes, massa="Fina", molho="Branco", queijo="Prato", cobertura="Calabresa"))
